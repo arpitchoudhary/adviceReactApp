4c6f5a1b23529d9820e59955bf2c285e4b565164
Arpit <arpit.choudhary@timesinternet.in>
2020-05-21 01:16:40 +0530

    update react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
+import LoadingOverlay from 'react-loading-overlay';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = { advice: ' ' }
+
+  componentDidMount() {
+    this.fetchAdvice()
+  }
+
+  fetchAdvice = () => {
+    console.log('fetching advice...')
+
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        console.log('gotcha advice...')
+        const { advice } = response.data.slip;
+        this.setState({ advice: advice })
+      }).catch((err) => {
+        console.log('advice fetch failed...')
+        console.log(err)
+      })
+
+  }
+
+  render() {
+
+    const { advice } = this.state
+    return (
+      <div className="app">
+        <div className="card">
+          {advice === ' ' ? <h1 className="heading">Loading....</h1> : <h1 className="heading">{advice}</h1>}
+          <button className="button" onClick={() => this.fetchAdvice()}>Give ME ADVICE!</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
